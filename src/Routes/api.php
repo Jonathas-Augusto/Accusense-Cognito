e9d705175a31d42befe75512b286fb0b2ca78e54
jonathas Augusto <jonathas_a.a@hotmail.com>
2022-08-23 18:12:14 -0300

    Corrige rotas  - Feita a correção das rotas para usa-las conforme o Laravel

--- src/Routes/api.php
+++ src/Routes/api.php
@@ -6,10 +6,10 @@ use Accusense\Cognito\Controllers\LoginController;
 use Illuminate\Support\Facades\Route;
 
 Route::controller(LoginController::class)->group(function () {
-    Route::post('login');
-    Route::post('first-login');
-    Route::post('refresh-token');
-    Route::post('revoke-token');
-    Route::get('reset-code');
-    Route::post('reset-password');
+    Route::post('/login', 'login');
+    Route::post('/first-login', 'firstLogin');
+    Route::post('/refresh-token', 'refresh');
+    Route::post('/revoke-token', 'revoke');
+    Route::get('/reset-code', 'sendResetCode');
+    Route::post('/reset-password', 'ResetPassword');
 });
